6202575bd342b47b95285286aaf0422559f4f39a
haruki20000416 <h.sato.0416@gmail.com>
2022-01-12 22:08:42 +0900

    投稿の削除

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,6 +11,12 @@
     <body>
       <h1 class="title">
             {{ $post->title }}
+            <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+                @csrf
+                @method('DELETE')
+            <button type="submit">delete</button> 
+       　　  </form>
         </h1>
         <div class="content">
             <div class="content__post">
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
-
+Route::get('/posts/{post}/edit', 'PostController@edit');
 Route::post('/posts', 'PostController@store');
+Route::put('/posts/{post}', 'PostController@update');
+Route::delete('/posts/{post}', 'PostController@delete');
